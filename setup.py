a0c55f2487793c7427532257778e4fac0176a62d
estevonal <estevon20al@gmail.com>
2026-04-09 14:20:58 -0300

    atualizando versão python

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='is_skeletons_detector',
+    name='mediapipe',
     version='0.1.0',
     description='MediaPipe Pose skeleton detection service for the IS ecosystem',
     url='https://github.com/Pseudo-nimo/tentativa_mediapipe',
@@ -9,7 +9,7 @@ setup(
     license='MIT',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    python_requires='>=3.10',
+    python_requires='>=3.0',
     entry_points={
         'console_scripts': [
             'is-skeletons-detector-stream=is_skeletons_detector.stream:main',
@@ -20,7 +20,8 @@ setup(
     install_requires=[
         'is-wire>=1.1.2',
         'is-msgs>=1.1.8',
-        'mediapipe>=0.10.0',
+        'mediapipe',
+        'opencensus-ext-zipkin==0.2.2',
         'opencv-python>=4.5.0',
         'python-dateutil',
     ],
